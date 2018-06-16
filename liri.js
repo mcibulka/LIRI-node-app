6874a45e039bae77df33a951c9f5dd0d698487d7
Matthew Cibulka <mcibulkadev@gmail.com>
2018-06-16 18:59:01 -0400

    Initial commit and setup; dotenv, twitter, node-spotify-api, and request packages added.

---
+++ liri.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+var keys = require("./keys.js");
+
+var request = require('request');
+
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
